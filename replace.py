a047c7164cd589d0ca2b8dc195df9f6429eb13b1
erionc <gh.user.one@gmail.com>
2021-03-08 10:37:33 +0100

    Ignored cache and test files

--- replace.py
+++ replace.py
@@ -24,7 +24,7 @@ def correction(field1, field2):
 def correction(field_in, field_out):
 	# get content from the first box
 	input_text = field_in.get() 
-	t = input_text
+	t = input_text ; total_sub = 0
 	
 	## all text corrections go here
 	
@@ -32,17 +32,28 @@ def correction(field_in, field_out):
 	# t = re.sub(r"eshte|ështe|eshtë", "është", t)
 	
 	## ç'kemi
-	t = re.sub(r"(c|c'|ç)(kemi)", r"ç'\2", t)
-	
+	t, c = re.subn(r"(c|c'|ç)(kemi)", r"ç'\2", t) ; total_sub += c
 	## Ç'kemi 
-	t = re.sub(r"(C|C'|Ç)(kemi)", r"Ç'\2", t)
+	t, c = re.subn(r"(C|C'|Ç)(kemi)", r"Ç'\2", t) ; total_sub += c
+	
+	## çfarë 
+	t, c = re.subn(r"(c|ç)(far)(e?)( |\.)", r"çfarë\4", t) ; total_sub += c
+	## Çfarë
+	t, c = re.subn(r"(C|Ç)(far)(e?)( |\.)", r"Çfarë\4", t) ; total_sub += c
 	
 	## Është
-	t = re.sub(r"(E|Ë)(sht)(e|ë)", r"Ë\2ë", t)
-	
+	t, c = re.subn(r"(E|Ë)(sht)(e|ë)", r"Ë\2ë", t) ; total_sub += c
 	## është
-	t = re.sub(r"(e|ë)(sht)(e|ë)", r"ë\2ë", t)
+	t, c = re.subn(r"(e|ë)(sht)(e|ë)", r"ë\2ë", t) ; total_sub += c
 	
+	## unë Unë ujë Ujë
+	t, c = re.subn(r"(u|U)(n|j)(e?)( |\.)", r"\1\2ë\4", t) ; total_sub += c
+	
+	## Mirë, mirë
+	t, c = re.subn(r"(Mir|mir)(e?)( |\.)", r"\1ë\3", t) ; total_sub += c
+
 	output_text = t
 	## insert the corrected text in the second box
 	field_out.insert(10, output_text)
+	## print number of total substitutions
+	print(f"U kryen {total_sub} zëvendësime.")
