d31b83e150d76718b27bfafa45547ea8120aa06a
erionc <gh.user.one@gmail.com>
2023-02-05 19:17:45 +0100

    Zgjerime të ç-ëve nistore

--- nistore_c.py
+++ nistore_c.py
@@ -5,10 +5,11 @@ from percaktime import *
 ## ruhen prapashtesat ndaj nuk pranohen tema me grupe alternative me |
 ## cafk, caj, cajnik, canak, cibuk, cift, cimk, cmim, co, corap, cudi, cun, cup 
 pa_c_nis = "afk|aj|ajnik|anak|akerdis|akmak|allm|arcaf|arçaf|art|ati|" + \
-"ibuk|ift|imk|izme|" + \
-"mend|mim|" + \
-"o|orap|orodit|" + \
-"udi|un"
+    "el|" + \
+    "ibuk|ift|imk|izme|" + \
+    "mend|mim|" + \
+    "o|orap|orodit|" + \
+    "udi|un"
 
 	
 	
