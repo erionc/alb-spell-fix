3a492c563f534551b3b05b2e8cdc0b57c5651898
erionc <gh.user.one@gmail.com>
2023-02-08 20:34:51 +0100

    Zgjerime të ç-ëve nistore

--- nistore_c.py
+++ nistore_c.py
@@ -4,8 +4,8 @@ from percaktime import *
 ## temat që shkruhen me C/c në vend të Ç/ç-së nistore
 ## ruhen prapashtesat ndaj nuk pranohen tema me grupe alternative me | si (c|ç)
 ## cafk, caj, cajnik, canak, cibuk, cift, cimk, cmim, co, corap, cudi, cun, cup 
-pa_c_nis = "afk|aj|ajnik|anak|akerdis|akmak|allm|arcaf|arçaf|art|ati|" + \
-    "el|ekan|ekic|ekiç|" + \
+pa_c_nis = "afk|aj|ajnik|anak|akerdis|akmak|allm|apkën|apken|arcaf|arçaf|art|ati|" + \
+    "el|ekan|ekic|ekiç|elik|" + \
     "ibuk|ift|imk|izme|" + \
     "mend|mim|mo|" + \
     "o|okollat|orap|orodit|" + \
