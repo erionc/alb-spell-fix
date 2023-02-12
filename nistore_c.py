44c55628c0508b18152357395090f09cb4ceb384
erionc <gh.user.one@gmail.com>
2023-02-12 19:04:16 +0100

    Zgjerime të ç-ëve nistore

--- nistore_c.py
+++ nistore_c.py
@@ -8,8 +8,8 @@ pa_c_nis = "afk|aj|ajnik|anak|akerdis|akmak|ale|alë|alo|alu|allm|apkën|apken|a
     "el|ekan|ekic|ekiç|elik|" + \
     "ibuk|ift|ikërrim|ikerrim|imk|izme|" + \
     "mend|mim|mo|" + \
-    "njer|" + \
-    "o|okollat|orap|orodit|" + \
+    "nder|njer|" + \
+    "o|okollat|orap|organi|orodit|" + \
     "u|udi|un"
 
 	
